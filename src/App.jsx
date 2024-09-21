743f1564faf76b8128279b8429fe0d37f08fe118
anushka1610-yadav <anushkaydv1610@gmail.com>
2024-09-21 23:12:33 +0530

    completed the about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Section/Navbar';
 import Hero from './Section/Hero';
+import About from './Section/About';
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
       <Hero />
+      <About />
     </main>
   )
 }
