325f5623a7a444fe6c5de85bb2ee93e3fca310f9
Anushka Yadav <anushkaydv1610@gmail.com>
2024-09-24 17:05:04 +0530

    added projects section's first part

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import Navbar from './Section/Navbar';
-import Hero from './Section/Hero';
-import About from './Section/About';
+import React from "react";
+import Navbar from "./Section/Navbar";
+import Hero from "./Section/Hero";
+import About from "./Section/About";
+import Projects from "./Section/Projects";
 
 const App = () => {
   return (
@@ -9,8 +10,9 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
